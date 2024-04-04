92f6bf8606f2653628bfc22e1e7337c796b43e87
jiehao045 <jiehaoyang@liceovvh.cl>
2024-04-04 12:04:55 -0300

    ok

---
+++ Fundamentos/04_Condicionales/01_Ejemplos/script.js
@@ -0,0 +1,13 @@
+console.log("Vinculado");
+
+function identificarMayorEdad() {
+    //Identificar si es mayor de edad
+    let edad = prompt("Por favor, ingrese su edad:");
+
+    if(edad >= 18){
+        alert("Es mayor de edad.");
+        } else {
+            alert("Es menor de edad.");
+        }
+
+}
