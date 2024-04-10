b9c7eb44879f709795e498c6af7e938d4ac210a2
jiehao045 <jiehaoyang@liceovvh.cl>
2024-04-10 11:35:48 -0400

    ok

--- Fundamentos/04_Condicionales/Ejercicios/04_Ejercicio/script.js
+++ Fundamentos/04_Condicionales/Ejercicios/04_Ejercicio/script.js
@@ -5,12 +5,12 @@ function numeroPositivoNegativo() {
     let numero = parseInt(prompt("Ingrese un número: "))
 
     if(numero > 0){
-        resultado = `El número <h2>${numero}</h2> es positivo.`
+        resultado = (`El número <h2>${numero}</h2> es positivo.`)
     }
     else if(numero <= -1)
-        resultado = `El número <h2>${numero}</h2> es negativo.`
-    else if(numero = 0){
-        resultado = `El número <h2>${numero}>/h2> es igual a cero.`
+        resultado = (`El número <h2>${numero}</h2> es negativo.`)
+    else if(numero == 0){
+        resultado = (`El número <h2>${numero}</h2> es igual a cero.`)
     }
     document.getElementById('lista').innerHTML = resultado;
 }
