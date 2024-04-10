9699b5f9cb7c4b1aa133da7513736428b4a2f05d
jiehao045 <jiehaoyang@liceovvh.cl>
2024-04-10 11:13:55 -0400

    ok

--- Fundamentos/04_Condicionales/Ejercicios/03_Ejercicio/script.js
+++ Fundamentos/04_Condicionales/Ejercicios/03_Ejercicio/script.js
@@ -4,12 +4,15 @@ function nombreUsuarioContraseña() {
     //Identificar si el usuario y contraseña son correctos
     let resultado = "";
     let nombreUsuario = prompt("Ingrese su nombre de usuario: ");
-    let _contraseña = prompt("Ingrese su contraseña: ");
+    let contraseña = prompt("Ingrese su contraseña: ");
+    
+    const usuario = "usuario123"
+    const password = "secreto"
 
-    if(nombreUsuario == usuario123 && _contraseña == secreto) {
-        resultado = `Acceso concedido.`
-        } else if(nombreUsuario != usuario123){
-        resultado = `Acceso denegado. Has ingresado el usuario incorrecto: <h2>${nombreUsuario}</h2>`
+    if(nombreUsuario == usuario && contraseña == password) {
+        resultado = `<h2>¡Acceso concedido!</h2>`
+        } else {
+        resultado = `<h2>Acceso denegado.</h2> `
         };
         
     document.getElementById('lista').innerHTML = resultado;
