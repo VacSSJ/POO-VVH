44e4686bba762b0c2037d2a1b1e5f62314a55a77
jiehao045 <jiehaoyang@liceovvh.cl>
2024-04-11 14:34:10 -0400

    ok

---
+++ Fundamentos/05_Bucles/Ejercicios/04_Ejercicios/script.js
@@ -0,0 +1,17 @@
+console.log("Vinculado");
+
+/* Escribe un programa que utilice un bucle for para mostrar el número mayor en una página HTML. */
+//Ejercicio con While
+function contarFor() {
+    let numeroMayor = parseInt(prompt("ingrese un número:"));
+    let array = [];
+    
+
+    for (let numeroMayor = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+    document.getElementById("lista").innerHTML = `<p>Suma del 0 al 10 con While:</p> <br><h2>${array}</h2>`;
+    }
+    
+
